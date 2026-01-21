86988221a0882a113f26760a0b62e50a478e9e32
東京紫苑 <11123-033@gs.machida-tky.ed.jp>
2026-01-21 12:38:42 +0900

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  darkMode: 'class',
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        youtube: {
+          red: '#FF0000',
+          dark: '#0F0F0F',
+          gray: '#272727',
+          light: '#F9F9F9'
+        }
+      }
+    }
+  }
+};
